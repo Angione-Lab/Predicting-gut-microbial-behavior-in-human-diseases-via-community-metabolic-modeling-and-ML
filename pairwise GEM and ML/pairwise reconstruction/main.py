6ec42d1d8a8a086bc9dda2a0c48efc14168565f6
CTarzi <78509617+CTarzi@users.noreply.github.com>
2026-03-03 10:22:15 +0000

    Update main.py

--- pairwise GEM and ML/pairwise reconstruction/main.py
+++ pairwise GEM and ML/pairwise reconstruction/main.py
@@ -1,4 +1,4 @@
-import o
+import os
 from MMinte.MMinte.site.widget4 import totalEXRxns,createEXmodel,createReverseEXmodel, addEXMets2SpeciesEX, replaceRxns,replaceMets,createCommunityModel,allPairComModels,createAllPairs
 from MMinte.MMinte.site.widget5 import calculateGR
 from MMinte.MMinte.site.widget6 import evaluateInteractions
